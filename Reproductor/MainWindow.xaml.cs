49c367d1853fccf95b4030905530c35043ab511d
javierconrod <javierin_kl@hotmail.com>
2020-01-20 16:43:12 -0700

    Slide con segundos

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ using Microsoft.Win32;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 
+using System.Windows.Threading;
+
 namespace Reproductor
 {
     /// <summary>
@@ -25,6 +27,9 @@ namespace Reproductor
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DispatcherTimer timer;
+
         //Lector de archivos
         AudioFileReader reader;
         //Comunicacion con la tarjeta de audio
@@ -37,6 +42,17 @@ namespace Reproductor
             btnReproducir.IsEnabled = false;
             btnDetener.IsEnabled = false;
             btnPausa.IsEnabled = false;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
+            sldReproduccion.Value = reader.CurrentTime.TotalSeconds;
         }
 
         void ListarDispositivosSalida()
@@ -80,15 +96,23 @@ namespace Reproductor
                     output.Play();
 
                     lblTiempoTotal.Text = reader.TotalTime.ToString().Substring(0,8);
+                    lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
+
                 }
             }
             btnReproducir.IsEnabled = false;
             btnDetener.IsEnabled = true;
             btnPausa.IsEnabled = true;
+
+            sldReproduccion.Maximum = reader.TotalTime.TotalSeconds;
+            sldReproduccion.Value = reader.CurrentTime.TotalSeconds;
+
+            timer.Start();
         }
 
         private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
         {
+            timer.Stop();
             reader.Dispose();
             output.Dispose();
         }
